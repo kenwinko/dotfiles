cabb9ba529c8f1f281bd2d50c17cc6727d857c3b
Ken Winko <kwinko@lakeheadu.ca>
2025-07-01 14:19:46 -0400

    various updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,21 +10,25 @@ Plug 'blazkowolf/gruber-darker.nvim'
 
 vim.call('plug#end')
 
+vim.cmd('filetype plugin indent on')
 vim.o.background = 'dark'
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.termguicolors = true
 vim.o.cursorline = true
+vim.o.cinoptions = 'l1'
 vim.o.softtabstop = 4
 vim.o.autoindent = true
 vim.o.fileformat = 'unix'
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.splitright = true
+vim.o.expandtab = true
 vim.o.encoding = 'utf-8'
 vim.o.textwidth = 79
 vim.o.shiftround = true
-vim.o.expandtab = true
+vim.o.splitright = true
+vim.o.incsearch = true
+vim.o.autochdir = true
 
 vim.cmd('colorscheme gruber-darker')
 
